1246802d95feb66264d5e7c6668feed3fdc260e4
Youssef Mostafa <youssefm32@nycstudents.net>
2026-01-05 10:18:11 -0500

    Update finalpractice.py

--- finalpractice.py
+++ finalpractice.py
@@ -2,17 +2,21 @@ class ThemeParkPass:
     def __init__(self, guest_name, pass_type, rides_used, ride_limit, is_valid):
         self.guest_name = guest_name
         self.pass_type = pass_type
-        self.rides_used = rides_used
+        self.rides_used = rides_used      # list of ride names
         self.ride_limit = ride_limit
-        self.is_valid = is_valid
+        self.is_valid = is_valid          # Boolean: True or False
 
-
-    def can_ride(ride_name, self):
-        if self.is_valid is "Not Valid" or self.rides_used >= self.ride_limit:
+    def can_ride(self, ride_name):
+        if not self.is_valid or len(self.rides_used) >= self.ride_limit:
             return False
         else:
             self.rides_used.append(ride_name)
             return True
-    can_ride("Scary_Coaster", "Bartholemew")
-        
-guest = ThemeParkPass("Bartholemew", "Premium", [1], 5, "Not Valid")
+
+
+# Create a guest pass
+guest = ThemeParkPass("Bartholemew", "Premium", [], 5, True)
+
+# Try to ride
+print(guest.can_ride("Scary_Coaster"))
+print(guest.rides_used)
